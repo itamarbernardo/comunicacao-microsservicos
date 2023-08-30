d6e2b6c2aa5e92eb7b87a1ae9731f46eeb0a9cc7
Itamar Bernardo <itamarbernardo2013@gmail.com>
2023-08-30 18:13:27 -0300

    Alterando lugar do endpoint de status da Sales-API pra nao precisar de autenticacao

--- sales-api/app.js
+++ sales-api/app.js
@@ -8,10 +8,6 @@ import { sendMessageToProductStockUpdateQueue } from './src/modules/product/rabb
 import orderRoutes from './src/modules/sales/routes/OrderRoutes.js'
 import tracing from './src/config/tracing.js';
 
-// connectMongoDb() 
-// createInitialData() //Cria os dados iniciais
-// connectRabbitMq() 
-
 const app = express()
 const env = process.env
 const PORT = env.PORT || 8082
@@ -41,39 +37,19 @@ app.get("/api/initial-data", async (req, res) => {
     return res.json({ message: "Data created." });
   });
 
-app.use(tracing) //A partir daqui, toda requisicao tem que ter um transactionId  
-app.use(checkToken) //A partir daqui, qualquer requisicao vai precisar conter o token Authorization com um token de acesso válido
-app.use(orderRoutes)
-// app.get('/teste', (req, res) => {
-//     try {
-//         sendMessageToProductStockUpdateQueue([
-//             {
-//                 productId: 1001,
-//                 quantity:2
-//             },
-//             {
-//                 productId: 1002,
-//                 quantity:3
-//             },
-//             {
-//                 productId: 1003,
-//                 quantity:4
-//             }
-//         ])        
-//         return res.status(200).json({ status: 200})
-//     } catch (error) {
-//         console.log(error)
-//         return res.status(500).json({ error: true})
-//     }
-// })
-
-app.get('/api/status', (req, res) => {
+  app.get('/api/status', (req, res) => {
     return res.status(200).json({
         service: "Sales-API",
         status: "up",
         httpStatus: 200
     })
 })
+
+app.use(tracing) //A partir daqui, toda requisicao tem que ter um transactionId  
+app.use(checkToken) //A partir daqui, qualquer requisicao vai precisar conter o token Authorization com um token de acesso válido
+app.use(orderRoutes)
+
+
 
 app.listen(PORT, () => {
     console.info(`Server Start Sucessfull at port ${PORT}`)
